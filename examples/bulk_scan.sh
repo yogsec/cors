2bc9ad89ae3bde58b6f15a1000845ca4a37d8c4f
Abhinav Singwal <abhinavsingwal@gmail.com>
2026-05-25 10:40:25 +0530

    Add files via upload

---
+++ examples/bulk_scan.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Example: Bulk scan with wordlist
+cors -w ../wordlists/common.txt -o results.txt -c 20
---
+++ examples/single_url.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Example: Single URL scan
+cors -u https://example.com
